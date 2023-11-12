42731390fb5c371debc482638dd03259ae070264
Jairoabarca <jairo.caravaca@hotmail.com>
2023-11-12 01:03:13 -0300

    adding forms and their style sheet

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
-
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
 
 
 
